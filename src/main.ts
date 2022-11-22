63b4a8221fb3127c64c329ff3516c551ca5ad30e
kpenfound <kyle@dagger.io>
2022-11-22 11:06:41 -0500

    broken jest tests
    Signed-off-by: kpenfound <kyle@dagger.io>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import Client, {connect} from '@dagger.io/dagger'
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const script: string = core.getInput('script')
+    core.debug(`Running ${script} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    connect(
+      async (client: Client) => {
+        // get reference to the local project
+        const source = await client.host().workdir(undefined, [script]).id()
+
+        // get Python image
+        const py = await client.container().from('python:3.9-slim').id()
+
+        // mount python script and execute
+        const runner = client
+          .container(py.id)
+          .withMountedDirectory('/src', source.id)
+          .withWorkdir('/src')
+          .exec(['python', script])
+
+        // get stdout
+        await runner.stdout().contents() // TODO - update sdk to get string out of stdout
+        //const stdout = 'success'
+      },
+      {LogOutput: process.stdout}
+    )
     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('stdout', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
