5d2dd70138659acb1907bec97b3c6688b2629a5d
kpenfound <kyle@dagger.io>
2022-11-28 21:38:14 -0800

    lint orb
    Signed-off-by: kpenfound <kyle@dagger.io>

--- src/scripts/action.sh
+++ src/scripts/action.sh
@@ -8,10 +8,10 @@ GITHUB_API=https://api.github.com/repos/${USER}/${REPO}/releases/latest
 ARCH=$(dpkg --print-architecture) 
 echo "Querying GitHub for the latest $ARCH release" 
 LATEST=$(curl -L -s -H 'Accept: application/json' $GITHUB_API) 
-LATEST_TAG=$(echo $LATEST | jq -r '.tag_name') 
+LATEST_TAG=$(echo "$LATEST" | jq -r '.tag_name') 
 echo "Found version $LATEST_TAG"
-LATEST_URL=$(echo $LATEST | jq -r ".assets[] | select(.name | contains(\"linux_$ARCH\")) | .url") 
+LATEST_URL=$(echo "$LATEST" | jq -r ".assets[] | select(.name | contains(\"linux_$ARCH\")) | .url") 
 echo "Downloading curl $LATEST_TAG from $USER"
-curl -vLJ -o action -H 'Accept: application/octet-stream' $LATEST_URL
+curl -vLJ -o action -H 'Accept: application/octet-stream' "$LATEST_URL"
 chmod +x action
 ./action
